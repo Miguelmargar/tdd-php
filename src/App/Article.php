2a7275ac07cc433074596cc7181a5732763ff0d3
miguelmargar <miguelmargar@outlook.com>
2021-03-29 12:21:50 +0100

    added first 3 tests within as tdd development process

---
+++ src/App/Article.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+class Article {
+    public $title;
+
+    public function getSlug() {
+        $slug = $this->title;
+
+        $slug = str_replace(' ', '_', $slug);
+
+        return $slug;
+    }
+}
---
+++ tests/ArticleTest.php
@@ -0,0 +1,29 @@
+<?php
+
+    use PHPUnit\Framework\TestCase;
+
+    class ArticleTest extends TestCase {
+        protected $article;
+
+        public function setUp(): void {
+            $this->article = new App\Article;
+        }
+
+        public function testTitleIsEmptyByDefault() {
+
+
+            $this->assertEmpty($this->article->title);
+        }
+
+        public function testSlugIsEmptyWithNoTitle() {
+
+
+            $this->assertSame($this->article->getSlug(), "");
+        }
+
+        public function testSlugHasSpacesReplacedByUnderscores() {
+            $this->article->title = "An example article";
+
+            $this->assertEquals($this->article->getSlug(), "An_example_article");
+        }
+    }
