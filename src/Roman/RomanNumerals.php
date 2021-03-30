e49fe19b48270f56bd9717dafd8fc86783a96790
miguelmargar <miguelmargar@outlook.com>
2021-03-30 11:38:25 +0100

    finished roman numerals translation with tdd process

--- src/Roman/RomanNumerals.php
+++ src/Roman/RomanNumerals.php
@@ -4,11 +4,48 @@ namespace Roman;
 
 class RomanNumerals {
 
-    public $romanNumber;
+    public $validRomanNums = ['I' => 1,
+                              'V' => 5,
+                              'X' => 10,
+                              'L' => 50,
+                              'C' => 100,
+                              'D' => 500,
+                              'M' => 1000];
 
-    public function getNumber(string $romanNumber) {
-        $this->romanNumber = $romanNumber;
-        return 1;
+    public function convert(string $romanNum) {
+        // Ensure non capitals are valid
+        $romanNum = str_split(strtoupper($romanNum));
+
+        $finalNum = 0;
+        // go through the string given
+        foreach($romanNum as $key => $num) {
+            // IE. $num = X
+            // Ensures each Roman Number given is valid
+            if (array_key_exists($num, $this->validRomanNums)) {
+                // First letter will always need to be added
+                if ($key === 0) {
+                    $finalNum += $this->validRomanNums[$num];
+                } else {
+                    // If the Roman Number needs to be added
+                    if ($this->validRomanNums[$num] <= $this->validRomanNums[$romanNum[$key-1]]) {
+                        $finalNum += $this->validRomanNums[$num];
+                    // If the Roman Number needs to be substracted
+                    } else {
+                        // Make the Current number as it needs to be subtracted from previous number
+                        $current = $this->validRomanNums[$num] - $this->validRomanNums[$romanNum[$key-1]];
+                        // Subtract the previous number from the final number which was added when looking
+                        // into the prious number turn as it is now needed in current number
+                        $finalNum -= $this->validRomanNums[$romanNum[$key-1]];
+                        // Finally add current number to final count
+                        $finalNum += $current;
+                    }
+                }
+                // If Roman Number not valid return wrong input given
+            } else {
+                return 0;
+            }
+        }
+        return $finalNum;
     }
 
 }
--- tests/RomanNumeralsTest.php
+++ tests/RomanNumeralsTest.php
@@ -4,29 +4,52 @@
 
     class RomanNumeralsTest extends TestCase {
 
-        protected static $roman;
+        public $roman;
 
-        protected static $validRomanNums;
-
-        protected static $validNormalNums;
+        public $validRomanNums;
 
         // Set up a new class for each test
         public function setUp(): void {
             // no need to load class file for Queue due to autoload
-            static::$roman = new Roman\RomanNumerals;
-            static::$validRomanNums = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
-            static::$validNormalNums = [1, 5, 10, 50, 100, 500, 1000];
+            $this->roman = new Roman\RomanNumerals;
         }
 
-        public function testIntegerIsReturned() {
-            $this->assertIsInt(static::$roman->getNumber(1));
-        }
-
-        public function testValidRomanNumsArePassedAsArguments() {
-            $input = static::$roman->getNumber('xyI');
-            $inputToArray = str_split($input);
-            foreach($inputToArray as $num) {
-            $this->assertStringContainsStringIgnoringCase($num, implode(static::$validRomanNums));
+        public function testAnIntIsReturned() {
+            foreach($this->roman->validRomanNums as $key => $num) {
+                $this->assertIsInt($this->roman->convert($key));
             }
         }
+
+        public function testCorrectTranslationForSingleNumIsReturned() {
+            foreach($this->roman->validRomanNums as $key => $num) {
+                $this->assertEquals($num, $this->roman->convert($key));
+            }
+        }
+
+        public function testCorrectTranslationForMultipleNumsIsReturned() {
+            $this->assertEquals(1005, $this->roman->convert('MV'));
+        }
+
+        public function testIncorrectSingleRomanNumberGivenAsArgumentReturnsCero() {
+            $this->assertEquals(0, $this->roman->convert('G'));
+        }
+
+        public function testIncorrectMultipleRomanNumbersGivenAsArgumentReturnsCero() {
+            $this->assertEquals(0, $this->roman->convert('GJ'));
+        }
+
+        public function testIncorrectAndCorrectMultipleRomanNumbersGivenAsArgumentReturnsZero() {
+            $this->assertEquals(0, $this->roman->convert('XJ'));
+        }
+
+        public function testSubstractingRomanNumeralsTranslation() {
+            $this->assertEquals(2014, $this->roman->convert('MMXIV'));
+            $this->assertEquals(19, $this->roman->convert('XIX'));
+            $this->assertEquals(4, $this->roman->convert('IV'));
+        }
+
+        public function testNonCapitalRomanNumbersArgumentProvided() {
+            $this->assertEquals(2021, $this->roman->convert('mMxXI'));
+        }
+
     }
